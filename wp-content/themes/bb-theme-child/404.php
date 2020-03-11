035d2433ece003f1414cfa60d887f1fd38efaaaf
ss-designs <design@suzistranahan.com>
2020-03-11 09:24:17 -0700

    Added 404 page

---
+++ wp-content/themes/bb-theme-child/404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="fl-content-full container">
+	<div class="row wrong-page">
+		<h2>Sorry, we sent you to the wrong place. </h2>
+		<a class="button" href="<?php echo home_url(); ?>"><i class ="fa fa-arrow-left"></i> Back to Home</a>
+		<h3>Please let our webmaster know there is a problem. </h3>
+		<a class="button" href="mailto:design@suzistranahan.com">Click Here <i class ="fa fa-arrow-right"></i> </a>
+	
+				
+	</div>
+</div>
+
+<?php get_footer(); ?>
