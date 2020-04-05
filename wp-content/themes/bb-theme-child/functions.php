99ec5c9487ee6178a3725df26f5c91b7dc681c9b
ss-designs <design@suzistranahan.com>
2020-04-05 06:14:59 -0700

    added exosome testimonial page

--- wp-content/themes/bb-theme-child/functions.php
+++ wp-content/themes/bb-theme-child/functions.php
@@ -10,6 +10,19 @@ require_once 'classes/class-fl-child-theme.php';
 // Actions
 add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
 
+// Change Pods MORE FIELDS Metabox Title to Custom
+
+add_filter('pods_meta_default_box_title','ni_changethatname',10,5);
+
+function ni_changethatname($title, $pod, $fields, $type, $name ) {
+
+//  To change another pod, copy this function and stack it right below so it's all within the same function. ' 
+  $title = ($name=='exosomes') ? __('Exosomes Info', 'plugin_lang') : $title ;
+  
+  
+  return $title;
+}
+
 
 
 
